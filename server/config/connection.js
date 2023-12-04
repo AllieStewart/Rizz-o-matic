691423f93150360b994bc059b80c061554efb5ae
AllieStewart <megaxis1217@gmail.com>
2023-12-04 11:28:46 -0600

    added base seeds in /seeders, they can connect to MongoDB if 'npm run seed' is ran and your connection.js either has your MongoDB connection string implemented there or elsewhere

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@
 // Connects to MongoDB database.
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts');
+mongoose.connect(process.env.MONGO_DB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts');
 
 module.exports = mongoose.connection;
 // End of JS file
