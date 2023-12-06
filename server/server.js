d0c8f8280b008d6f01e2c5a9f64b5cfa0c946fa3
vincula1 <141301612+vincula1@users.noreply.github.com>
2023-12-05 19:50:53 -0600

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,62 +1,21 @@
-// Start of JS file
-// Main file for launching back-end responsibilities.
 require('dotenv').config();
-
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
-const { authMiddleware } = require('./utils/auth');
 const { generateRizz } = require('./utils/openaiService');
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
 const app = express();
-
-
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
 
 app.get('/generate-quote', async (req, res) => {
   try {
     const rizz = await generateRizz();
+    console.log('Generated Rizz:', rizz);
     res.json({ quote: rizz });
   } catch (error) {
+    console.error('Error in /generate-quote route:', error);
     res.status(500).send(error.message);  
   }
 });
 
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async () => {
-  await server.start();
-
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
-
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
-  });
-};
-
-// Call the async function to start the server
-  startApolloServer();
-// End of JS file
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
